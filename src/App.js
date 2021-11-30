53ac1ae20698e749463d13894eef1504717d22ec
Eric Esteban <eric_7@hotmail.com>
2021-11-30 18:21:53 +0100

    done 4 it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import contacts from "./contacts.json";
+
+const remainingContacts = [...contacts];
+const initAgenda = remainingContacts.splice(0, 5);
 
 function App() {
+  const [agenda, setAgenda] = useState(initAgenda);
+  const addContact = () => {
+    const randomIndex = Math.floor(Math.random() * remainingContacts.length);
+    const randomContact = remainingContacts.splice(randomIndex, 1);
+    // const randomContact = randomContact[randomIndex] // It will also return a random contact, but won't modify the original array.
+    setAgenda(agenda.concat(randomContact));
+    // setAgenda([...agenda, randomContact]);
+  };
+
+  const sortByName = () => {
+    const sortedArrName = agenda.sort((a, b) => {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      return 0;
+    }).slice();
+    setAgenda(sortedArrName);
+    console.log("sortedArrName", sortedArrName);
+  };
+
+  const sortByPopularity = () => {
+    const sortedArrPopu = agenda.sort((a, b) => {
+      if (a.popularity > b.popularity) {
+        return -1;
+      }
+      if (a.popularity < b.popularity) {
+        return 1;
+      }
+      return 0;
+    }).slice();
+    setAgenda(sortedArrPopu);
+    console.log("sortedArrPopu", sortedArrPopu);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>IRONCONTACTS</h1>
+      <button onClick={addContact}>Add contact</button>
+      <button onClick={sortByName}>Sort By Name</button>
+      <button onClick={sortByPopularity}>Sort By Popularity</button>
+
+      <div className="App">
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Oscar Winner?</th>
+              <th>Emmy Winner?</th>
+            </tr>
+          </thead>
+          <tbody>
+            {agenda.map((contacts) => {
+              return (
+                <tr key={contacts.id}>
+                  <td>
+                    <img
+                      src={contacts.pictureUrl}
+                      alt={contacts.name}
+                      width="100px"
+                      height="150px"
+                    />
+                  </td>
+                  <td>{contacts.name}</td>
+                  <td>{contacts.popularity.toFixed(2)}</td>
+                  <td>{contacts.wonOscar ? "🏆" : "X"}</td>
+                  <td>{contacts.wonEmmy && "🏆"}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
